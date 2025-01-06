670d1a6e0b613270d2c65051d22b516a336e41cd
Eugene Agesa <eugenethebandit@gmail.com>
2025-01-06 05:33:15 +0300

    updated the settings.py to handle static files well

--- project/settings.py
+++ project/settings.py
@@ -99,7 +99,7 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'project', 'static'),  # Update this path to match your structure
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
